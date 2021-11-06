5b13bc486d77464475b2c6588561adc660837c7b
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-06 22:09:31 +0200

    adds Task-9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const bodyRef = document.body;
+const buttonRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+const bodyChangeColor = event => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+};
+
+buttonRef.addEventListener('click', bodyChangeColor);
