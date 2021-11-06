1ac3c114450845815ca989568739c3261b9aa245
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-06 19:15:03 +0200

    adds Task-8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        mail,
+        password
+    };
+    
+    if (mail === "" || password === "") {
+        const message = "Все поля должны быть заполнены!";
+        alert(message);
+    };
+    console.log(formData);
+
+    event.currentTarget.reset();
+
+   // Решение через класс FormData ниже
+    // const formData = new FormData(event.currentTarget);
+    // console.log(formData);
+
+    // formData.forEach((value, name) => {
+    //     console.log("value:", value);
+    //     console.log("name:", name);
+    // })
+};
+
+form.addEventListener('submit', onFormSubmit);
